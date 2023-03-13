6f143afd95d185facee14912e9a4dc06a6a79a80
SebuBergman <basti.bergman@gmail.com>
2023-03-13 18:23:30 +0200

    Added cors to index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const db = require("./models/");
+const cors = require("cors");
 
+app.use(cors())
 app.use(bodyParser.json());
 
 function success(res, payload) {
