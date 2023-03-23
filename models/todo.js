47d6c029af60cb3577024de0e03fdb117a5c1e3e
SebuBergman <basti.bergman@gmail.com>
2023-03-23 13:11:15 +0200

    deleted date for backend. I don't think i need it

--- models/todo.js
+++ models/todo.js
@@ -3,18 +3,14 @@ const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema({
   // creating schemas for todos
   task: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
+        type: String,
+        unique: true,
+        required: true
+    },
+  completed:{
+      type: Boolean,
+      default: false
+  }
 });
 
 const todoModel = mongoose.model("Todo", todoSchema);
